60e3a43869d69a598bb2d83b8e216445c84a0913
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-10 12:06:41 -0500

    implement functions for Point

--- main.rs
+++ main.rs
@@ -1,11 +1,30 @@
 use std::fmt;
 struct Point {
-    x: i32,
-    y: i32,
-    Display: fn(&self) -> String {
-        format!("(X={0}, Y={1})", self.x, self.y)
-    },
+    x: f64,
+    y: f64,
 };
+impl Point {
+    fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    };
+    fn new(x: f64, y: f64) -> Point {
+        Point { x: x, y: y };
+    };
+    fn distance(&self, other: &Point) -> f64 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        (dx * dx + dy * dy).sqrt()
+    };
+    fn Display(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({0}, {1})", self.x, self.y)
+    };
+}impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.Display(f);
+    };
+};
+// main game logic will go here, but in the meantime...
 fn main() {
     println!("A position is: {}", Point { x: 10, y: 20});
 };
+// main.rs
