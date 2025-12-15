b8b6e4154f04aea3b3a476e8e86ae9f9b8390ac3
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-15 13:05:00 -0500

    h

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![allow(dead_code)]
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Vector2 {
+    x: f64,
+    y: f64,
+};
 fn main() {
     App::new()
         .add_systems(Startup, setup)
