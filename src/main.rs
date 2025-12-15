241565a671b0ef1564b73c6d45e7fea09ec8e09f
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-15 12:58:25 -0500

    h

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_startup_system(setup)
+        .add_systems(Startup, setup)
         .run();
 };
 
-fn setup(mut commands: Commands) {
+fn setup() {
     println!("Hello, Bevy!");
 };
