760bb560ad8bb65797d37d5f7b953b083ca7cf94
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-18 08:20:05 -0500

    h

--- src/main.rs
+++ src/main.rs
@@ -50,30 +50,6 @@ struct Rectangle {
     height: f64,
     rotation: f64,
 }
-impl Rectangle {
-    pub fn create(x1: f64, y1: f64, x2: f64, y2: f64, position: Vector2) -> Rectangle {
-        Rectangle {
-            width: (x2 - x1).abs(),
-            height: (y2 - y1).abs(),
-            rotation: 0.0,
-        }
-    }
-    pub fn area(&self) -> f64 {
-        self.width * self.height
-    }
-    pub fn perimeter(&self) -> f64 {
-        2.0 * (self.width + self.height)
-    }
-    pub fn center(&self) -> Vector2 {
-        Vector2 {
-            x: (self.width) / 2.0,
-            y: (self.height) / 2.0,
-        }
-    }
-    pub fn draw(&self) {
-        // placeholder for drawing logic
-    }
-}
 
 fn main() {
     App::new()
