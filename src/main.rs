b4191802d24460ebd4a5dc76055a779256e46757
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-18 08:17:48 -0500

    rectangle ported to Bevy

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,75 @@ use bevy::prelude::*;
 struct Vector2 {
     x: f64,
     y: f64,
-};
+}
+impl Vector2 {
+    pub fn origin() -> Vector2 {
+        Vector2 { x: 0.0, y: 0.0 }
+    }
+    pub fn new(x: f64, y: f64) -> Vector2 {
+        Vector2 { x: x, y: y }
+    }
+    pub fn distance(&self, other: &Vector2) -> f64 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        (dx * dx + dy * dy).sqrt()
+    }
+    pub fn offset(&self, other: &Vector2) -> Vector2 {
+        Vector2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+    pub fn Display(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({0}, {1})", self.x, self.y)
+    }
+}
+impl fmt::Display for Vector2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.Display(f)
+    }
+}
+// AND IT BEGINS!
+#[derive(Component)]
+struct Name(String);
+#[derive(Component)]
+struct Position(Vector2);
+#[derive(Component)]
+struct Velocity(Vector2);
+#[derive(Component)]
+struct Acceleration(Vector2);
+#[derive(Component)]
+struct Mass(f64);
+#[derive(Component)]
+struct Rectangle {
+    width: f64,
+    height: f64,
+    rotation: f64,
+}
+impl Rectangle {
+    pub fn create(x1: f64, y1: f64, x2: f64, y2: f64, position: Vector2) -> Rectangle {
+        Rectangle {
+            width: (x2 - x1).abs(),
+            height: (y2 - y1).abs(),
+            rotation: 0.0,
+        }
+    }
+    pub fn area(&self) -> f64 {
+        self.width * self.height
+    }
+    pub fn perimeter(&self) -> f64 {
+        2.0 * (self.width + self.height)
+    }
+    pub fn center(&self) -> Vector2 {
+        Vector2 {
+            x: (self.width) / 2.0,
+            y: (self.height) / 2.0,
+        }
+    }
+    pub fn draw(&self) {
+        // placeholder for drawing logic
+    }
+}
 
 fn main() {
     App::new()
