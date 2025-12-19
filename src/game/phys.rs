694b7aed9f7a6a5fb05b44d7a235e7cb9f3b56ca
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2025-12-19 09:42:56 -0500

    phys

--- src/game/phys.rs
+++ src/game/phys.rs
@@ -36,3 +36,45 @@ impl fmt::Display for Vector2 {
         self.Display(f)
     }
 }
+
+#[derive(Component)]
+pub struct PhysicsBody {
+    pub position: Vector2,
+    pub velocity: Vector2,
+    pub mass: f64,
+}
+impl PhysicsBody {
+    pub fn new(position: Vector2, velocity: Vector2, mass: f64) -> PhysicsBody {
+        PhysicsBody {
+            position: position,
+            velocity: velocity,
+            mass: mass,
+        }
+    }
+    pub fn apply_force(&mut self, force: &Vector2, delta_time: f64) {
+        let acceleration = Vector2 {
+            x: force.x / self.mass,
+            y: force.y / self.mass,
+        };
+        self.velocity.x += acceleration.x * delta_time;
+        self.velocity.y += acceleration.y * delta_time;
+    }
+    pub fn update_position(&mut self, delta_time: f64) {
+        self.position.x += self.velocity.x * delta_time;
+        self.position.y += self.velocity.y * delta_time;
+    }
+}
+
+#[derive(Component)]
+pub struct Collider {
+    pub radius: f64,
+}
+impl Collider {
+    pub fn new(radius: f64) -> Collider {
+        Collider { radius: radius }
+    }
+    pub fn is_colliding(&self, other: &Collider, pos_a: &Vector2, pos_b: &Vector2) -> bool {
+        let distance = pos_a.distance(pos_b);
+        distance < (self.radius + other.radius)
+    }
+}
