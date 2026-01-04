d2ba8a24f720761a610c20ce25b7fb69c42e586b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-01-03 21:38:39 -0500

    h

--- src/game.rs
+++ src/game.rs
@@ -5,15 +5,16 @@ use bevy::window::{Window, WindowMode};
 pub struct GameState{
     score: u32,
     level: u32,
-    window: Option<Window>
+    window: *mut Window,
 } impl GameState {
     pub fn new(window: &mut Window) -> Self {
         Self {
             score: 0,
             level: 1,
-            window:Some(window.clone()),
+            window:window,
         }
     }
+
     // Increases the score by a given number of points
     pub fn increase_score(&mut self, points: u32) {
         self.score += points;
@@ -22,9 +23,17 @@ pub struct GameState{
     pub fn next_level(&mut self) {
         self.level += 1;
     }
+    // Gets the window from the reference
+    pub fn get_window(&self) -> &mut Window {
+        if (self.window.is_null()) {
+            panic!("Window pointer is null!");
+        };
+        // Safety: We ensure that the window pointer is valid before dereferencing (its required to be valid for the struct to initialize)
+        unsafe { &mut *self.window }
+    }
     // Sets the window title of the attached window
     pub fn set_window_title(&mut self, title: String) {
-        let window: &mut Window=self.window.as_mut().unwrap();
-        window.title = title;
+        let window_ref: &mut Window = self.get_window();
+        window_ref.title = title;
     }
 }
