151c570773bdffd35377ce527e5fa12e23e721fe
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2026-02-02 12:21:35 -0500

    serializer

--- src/serialization.rs
+++ src/serialization.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 use std::{fs::File, io::{self, Read, Write}, path::Path};
 use std::error::Error;
+mod game;
 use crate::game::Level;
 
 fn serialize_level_data(level: &Level, path: &Path) -> io::Result<()> {
@@ -9,4 +10,16 @@ fn serialize_level_data(level: &Level, path: &Path) -> io::Result<()> {
 
 fn deserialize_level_data() {
     // placeholder for deserialization logic
+}
+
+pub struct Serializer;
+impl Serializer {
+    pub fn save_level(level: &Level, path: &Path) -> Result<(), Box<dyn Error>> {
+        serialize_level_data(level, path)?;
+        Ok(())
+    }
+    pub fn load_level(path: &Path) -> Result<Level, Box<dyn Error>> {
+        let level = deserialize_level_data()?;
+        Ok(level)
+    }
 }
